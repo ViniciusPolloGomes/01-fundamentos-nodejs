b6527b82c6f5b96090ece871f2bc4e67f66dd67e
viniciuspollogomes <viniciuspollogomes@gmail.com>
2026-01-08 22:42:18 -0300

    Criando estrutura de rotas do backend
    Principais métodos HTTP implementados ,
    
    Salvando dados em memória,
    
    Aprendendo definir cabeçarios JSON como resposta a front-end

--- src/server.js
+++ src/server.js
@@ -1,7 +1,24 @@
 import http from 'node:http'
 
+const users = []
 const server = http.createServer((req, res)=>{
+    const {method, url} = req
 
+    //early return ou seja nada do que tiver abaixo do retun será executado , por isso não usamos else
+    if(method==='GET' && url ==='/users'){
+        return res
+            .setHeader('Content-type','application/json ')
+            .end(JSON.stringify(users))
+    }
+
+    if(method==='POST' && url ==='/users'){
+        users.push({
+            id:1,
+            name:'Jonh Doe',
+            email:'jonhdoe@example.com'
+        })
+        return res.end('Criação de usuários')
+    }
     return res.end('Hello ignite')
 
 })
